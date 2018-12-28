c6c0d1bb66dd6ab0e2f83970b64520d96589f25f
MNalbant <m.nalbant042@gmail.com>
2018-12-28 15:09:02 +0100

    Add project files.

---
+++ TestAPIserver/Migrations/20181221165838_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestAPIserver.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Mail = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ TestAPIserver/wwwroot/scripts/apiscripts.js
@@ -0,0 +1,26 @@
+function func1() {
+    var user = {
+        FirstName: $("#FirstName").val(),
+        LastName: $("#LastName").val(),
+        Mail: $("#Email").val(),
+        Password: $("#Password").val()
+    };
+    var l = JSON.stringify(user);
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://localhost:44387/api/users",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache",
+        },
+        "processData": false,
+        "data": l
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
